1ec1c086c3e4b60e06beceed42dca54e809e1ecd
Palomon <pm07.privatemonster@gmail.com>
2017-07-06 17:53:26 +0700

    form_update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react';
 import './asset/css/App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      C: 0,
+      F: 0,
+    };
+  }
+
+  changeCeltoFah(i){
+    console.log(this.refs.Celsius.value)
+  }
+
   render() {
     return (
       <div className="container">
@@ -9,14 +21,21 @@ class App extends Component {
           <div className="col-md-6 col-md-offset-3">
             <div className="card">
               <h3>Hello</h3>
+              <hr/>
               <form className="form-horizontal">
-                <label>Celsius
-                  <input type="text" className="form-control"/>
-                </label>
-                <label>Fahrenheit
-                  <input type="text" className="form-control"/>
-                </label>
+                <div className="form-group">
+                  <label>Celsius
+                    <input type="text" ref="Celsius" className="form-control" onChange={() => this.changeCeltoFah()}/>
+                  </label>
+                </div>
+                <div className="form-group">
+                  <label>Fahrenheit
+                    <input type="text" className="form-control"/>
+                  </label>
+                </div>
               </form>
+              <hr/>
+              <h4>Thanks</h4>
             </div>
           </div>
         </div>
